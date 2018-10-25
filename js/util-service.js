79393d7901c5f53e070df95934a61fd32d524fd1
Jake Collins <jake.james.collins@hotmail.com>
2018-10-25 20:10:53 +0300

    css a bit

--- js/util-service.js
+++ js/util-service.js
@@ -8,16 +8,3 @@ function getFromStorage(key) {
 function saveToStorage(key, val) {
     localStorage.setItem(key, JSON.stringify(val))
 }
-
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
